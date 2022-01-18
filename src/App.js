4a0836363a2d1f1aff76e39ec8f0669d57459a71
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-01-18 05:43:03 +0100

    pagination reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import Follower from './Follower'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loading, data } = useFetch()
+    const [page, setPage] = useState(0)
+    const [followers, setFollowers] = useState([])
+
+    useEffect(() => {
+        if (loading) return
+        setFollowers(data[page])
+    }, [loading, page])
+
+
+    const handlePage = (index) => {
+        setPage(index)
+    }
+
+    const nextPage = () => {
+        setPage((oldPage) => {
+            let nextPage = oldPage + 1
+            if(nextPage > data.length - 1){
+                nextPage = 0
+            }
+            return nextPage
+        })
+    }
+
+    const prevPage = () => {
+        setPage((oldPage) => {
+            let prevPage = oldPage - 1
+            if(prevPage < 0){
+                prevPage = data.length-1
+            }
+            return prevPage
+        })
+    }
+
+    return (
+        <main>
+            <div className='section-title'>
+                <h1>{loading ? 'loading...' : 'pagination'}</h1>
+                <div className='underline'></div>
+            </div>
+            <section className='followers'>
+                <div className='container'>
+                    {followers .map((follower) => {
+                        return <Follower key={follower.id} {...follower} />
+                    })}
+                </div>
+                {!loading && <div className='btn-container'>
+                    <div className='prev-btn' onClick={prevPage}>prev</div>
+                    {data.map((item, index) => {
+                        return (
+                            <button className={`page-btn ${index === page ? 'active-btn' : null }`} key={index} onClick={() => handlePage(index)}>
+                                {index + 1}
+                            </button>
+                        )
+                    })}
+                    <div className='prev-btn' onClick={nextPage}>Next</div>
+                </div>}
+            </section>
+        </main>
+    )
 }
 
-export default App;
+export default App
